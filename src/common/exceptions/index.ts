4dedfd4be2aa4e7d5fe8cadf28d208d3a396e253
luizxsoto <luizxsoto@gmail.com>
2022-10-29 17:47:20 -0300

    feat: add unauthorized exception

--- src/common/exceptions/index.ts
+++ src/common/exceptions/index.ts
@@ -1,3 +1,4 @@
 export * from './application';
+export * from './unauthorized';
 export * from './unprocessable-entity';
 export * from './validation';
---
+++ src/common/exceptions/unauthorized.ts
@@ -0,0 +1,14 @@
+import { StatusCodes } from '@/common/constants';
+import { i18n } from '@/common/i18n';
+
+import { ApplicationException } from './application';
+
+export class UnauthorizedException extends ApplicationException {
+  constructor() {
+    super({
+      name: 'UnauthorizedException',
+      code: StatusCodes.UNAUTHORIZED,
+      message: i18n().common.exceptions.unauthorizedException,
+    });
+  }
+}
